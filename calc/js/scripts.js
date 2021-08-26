aee2e3f659599ce6e82fec13b5ba17d6b808cef3
Nathaniel Lacerda <nathaniel.lacerda@synchro.com.br>
2021-08-26 14:47:40 -0300

    Calculadora monetária

---
+++ calc/js/scripts.js
@@ -0,0 +1,32 @@
+const data = [
+    {key: 0, title: 'Débito', parcels: 1, percent: 2},
+    {key: 1, title: '1x', parcels: 1, percent: 4},
+    {key: 2, title: '2x', parcels: 2, percent: 5},
+    {key: 3, title: '3x', parcels: 3, percent: 5},
+    {key: 4, title: '4x', parcels: 4, percent: 6},
+    {key: 5, title: '5x', parcels: 5, percent: 6},
+    {key: 6, title: '6x', parcels: 6, percent: 7},
+    {key: 7, title: '7x', parcels: 7, percent: 7},
+    {key: 8, title: '8x', parcels: 8, percent: 8},
+    {key: 9, title: '9x', parcels: 9, percent: 8},
+    {key: 10, title: '10x', parcels: 10, percent: 9},
+    {key: 11, title: '11x', parcels: 11, percent: 9},
+    {key: 12, title: '12x', parcels: 12, percent: 10},
+];
+
+const calcular = () => {
+    const value = parseFloat($('#value').val());
+    const parcels = data[$('#parcel').val()].parcels;
+    const percent = data[$('#parcel').val()].percent;
+    console.log(parcels + ' parcelas: ' + percent + '%');
+
+    debugger;
+    let increment = (percent / 100) * value;
+    let result = (value + increment) / (parcels || 1);
+
+    $('#result').html(isNaN(result) ? '-' : 'R$ ' + result.toFixed(2).replace('.', ','));
+}
+
+$(document).ready(() => {
+    data.map(item => $('#parcel').append(item.title, `<option value="${item.key}">${item.title}</option>`));
+});
